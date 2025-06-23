0ea14e4006abe0388e04e0d9940c8614c977aad2
Mayuresh Durgule <mayureshd1303@gmail.com>
2025-06-23 17:39:55 +0530

    bot update

--- client/src/components/AskBhideChat.jsx
+++ client/src/components/AskBhideChat.jsx
@@ -38,7 +38,7 @@ const AskBhideChat = () => {
     setCooldown(true);
 
     try {
-      const res = await axios.post("http://localhost:5000/api/ask", {
+      const res = await axios.post(`${import.meta.env.VITE_BOT_API}`, {
         question: input,
       });
 
